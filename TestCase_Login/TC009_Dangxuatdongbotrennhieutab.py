d75061a657040edce194d8a20a7e8dce5f82e8c1
atuankia205 <atuankia205@gmail.com>
2025-12-17 15:27:21 +0700

    Add files via upload

---
+++ TestCase_Login/TC009_Dangxuatdongbotrennhieutab.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, time, re
+
+class TC009NgXuTNgBTrNNhiUTab(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome(executable_path=r'')
+        self.driver.implicitly_wait(30)
+        self.base_url = "https://www.google.com/"
+        self.verificationErrors = []
+        self.accept_next_alert = True
+    
+    def test_t_c009_ng_xu_t_ng_b_tr_n_nhi_u_tab(self):
+        driver = self.driver
+        driver.get("http://hauiproj.somee.com/Default.aspx")
+        driver.find_element_by_id("LinkDX").click()
+        driver.execute_script("window.open(\"http://hauiproj.somee.com/Default.aspx\",\"Tab2\")");
+    
+    def is_element_present(self, how, what):
+        try: self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e: return False
+        return True
+    
+    def is_alert_present(self):
+        try: self.driver.switch_to_alert()
+        except NoAlertPresentException as e: return False
+        return True
+    
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally: self.accept_next_alert = True
+    
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
+
+if __name__ == "__main__":
+    unittest.main()
